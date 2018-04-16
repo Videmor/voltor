9aa5e439aa4c17c2ca23128269b30ccf90f75ecb
videmor <videmor@gmail.com>
2018-04-16 02:11:52 -0500

    permission in circleci

--- deployments/prd.sh
+++ deployments/prd.sh
@@ -1,7 +1,7 @@
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.2/packer_1.2.2_linux_amd64.zip
 wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip?_ga = 2.142984049.552417044.1523847625-358755134.1523847625
-unzip /tmp/packer.zip -d /bin
-unzip /tmp/terraform.zip -d /bin
+sudo unzip /tmp/packer.zip -d /bin
+sudo unzip /tmp/terraform.zip -d /bin
 
 packer validate deployments/template.json &&
 packer build deployments/template.json &&
